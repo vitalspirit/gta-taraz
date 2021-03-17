cb2b43f7dc74563720afe6bcff1bbb1853ee8498
vitaliy <vitliymolchanov@mail.ru>
2021-03-17 09:49:24 +0600

    56955

--- gtu_stops/settings.py
+++ gtu_stops/settings.py
@@ -122,5 +122,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import dj_database_url
 
 db_from_env = dj_database_url.config()
-#DATABASES = { 'default': dj_database_url.config() }
-DATABASES['dfault'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config() }
+#DATABASES['dfault'].update(db_from_env)
